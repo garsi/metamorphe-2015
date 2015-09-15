18b035169c650f8158214cdebb6f956cc89c8d79
Kat Garsi <katgarsi@gmail.com>
2015-09-15 16:47:01 -0700

    Refactoring

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
-$('.work-hp').hover(
+$('.gallery-item img').hover(
 	function(){
 		$(this).find('.details').fadeOut(250);
 		$(this).find('.caption').fadeIn(250);
 	},
 	function(){
-		$(this).find('.caption').fadeOut(250);
-		$(this).find('.details').fadeIn(250);
+		$(this).find('.caption').fadeOut(50);
+		$(this).find('.details').fadeIn(50);
 	}
 );
 
